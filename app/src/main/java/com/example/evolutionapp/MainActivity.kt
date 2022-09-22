eff999177c13868cbad3c7951d98dbe1302166ee
Joaquin Alfonso San Martin Alvarez <u201910511@upc.edu.pe>
2022-09-22 16:02:49 -0700

    feat: base project

--- app/src/main/java/com/example/evolutionapp/MainActivity.kt
+++ app/src/main/java/com/example/evolutionapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.evolutionapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    val btnLogin: Button = findViewById(R.id.btnLogin)
+    val btnSignup: Button = findViewById(R.id.btnSignup)
+
+    btnLogin.setOnClickListener(){
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    btnSignup.setOnClickListener(){
+        startActivity(Intent(this, SignupActivity::class.java))
     }
 }
