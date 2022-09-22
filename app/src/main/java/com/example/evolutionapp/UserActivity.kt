f63c089c2707e547348394876ff45272499859e8
Joaquin Alfonso San Martin Alvarez <u201910511@upc.edu.pe>
2022-09-22 16:24:24 -0700

    feat: select user activity

--- app/src/main/java/com/example/evolutionapp/UserActivity.kt
+++ app/src/main/java/com/example/evolutionapp/UserActivity.kt
@@ -1,11 +1,21 @@
 package com.example.evolutionapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        val btnAlumno: Button = findViewById(R.id.btnUserAlumno)
+        val btnDocente: Button = findViewById(R.id.btnUserDocente)
+
+
+        btnAlumno.setOnClickListener(){
+            startActivity(Intent(this, HomeAlumnoActivity::class.java))
+        }
     }
 }
