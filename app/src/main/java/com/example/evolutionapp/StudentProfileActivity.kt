be16fcf9b1291ccf97bbb96ace5abdb59c933d7a
Joaquin Alfonso San Martin Alvarez <u201910511@upc.edu.pe>
2022-09-24 19:04:37 -0700

    feat: student activity

--- app/src/main/java/com/example/evolutionapp/StudentProfileActivity.kt
+++ app/src/main/java/com/example/evolutionapp/StudentProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.evolutionapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class StudentProfileActivity : AppCompatActivity() {
@@ -11,9 +12,15 @@ class StudentProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_student_profile)
 
         val btnArrowback: ImageButton = findViewById(R.id.btnArrowback)
+        val btnProfile: Button = findViewById(R.id.btnChangeImage)
+        val btnLogOut: Button = findViewById(R.id.btnLogOut)
 
         btnArrowback.setOnClickListener(){
             startActivity(Intent(this, HomeStudentActivity::class.java))
+        }
+
+        btnLogOut.setOnClickListener(){
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
