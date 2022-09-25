d58f6e5f962c5a22939834b64a843e47e0c84125
Joaquin Alfonso San Martin Alvarez <u201910511@upc.edu.pe>
2022-09-25 13:04:38 -0700

    feat: nav bar added

--- app/src/main/java/com/example/evolutionapp/HomeStudentActivity.kt
+++ app/src/main/java/com/example/evolutionapp/HomeStudentActivity.kt
@@ -1,19 +1,59 @@
 package com.example.evolutionapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_home_student.*
 
 class HomeStudentActivity : AppCompatActivity() {
+
+    private lateinit var toogle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_student)
 
-        val btnProfile: ImageButton = findViewById(R.id.btnProfile)
+        toogle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        btnProfile.setOnClickListener(){
-            startActivity(Intent(this, StudentProfileActivity::class.java))
+        nav_view.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, HomeStudentFragment())
+                        commit()
+                    }
+                }
+
+                R.id.nav_courses -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, CoursesStudentFragment())
+                        commit()
+                    }
+                }
+
+                R.id.nav_announcements -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, AnnouncementStudentFragment())
+                        commit()
+                    }
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+            true
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toogle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/evolutionapp/LoginActivity.kt
+++ app/src/main/java/com/example/evolutionapp/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.evolutionapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin: Button = findViewById(R.id.btnLogin1)
+
+        btnLogin.setOnClickListener(){
+            startActivity(Intent(this, HomeStudentActivity::class.java))
+        }
     }
 }
